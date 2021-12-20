819ec8e8b1ec92d9e25702aa08afd355c01323f2
joshua <mama27j@gmail.com>
2021-12-20 22:54:29 +0100

    add support for large files and more options for formats

---
+++ create-hls-480p.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+ffmpeg -i  "$1" \
+-filter_complex \
+"[0:v]split=3[v0][v1][v2]; \
+[v0]scale=w=854:h=480[v1out]; [v1]scale=w=640:h=360[v2out]; [v2]scale=w=256:h=144[v3out]" \
+-map [v1out] -c:v:1 libx264 -x264-params "nal-hrd=cbr:force-cfr=1" -b:v:1 3M -maxrate:v:1 3M -minrate:v:1 3M -bufsize:v:1 3M -preset fast -g 48 -sc_threshold 0 -keyint_min 48 \
+-map [v2out] -c:v:2 libx264 -x264-params "nal-hrd=cbr:force-cfr=1" -b:v:2 1M -maxrate:v:2 1M -minrate:v:2 1M -bufsize:v:2 1M -preset fast -g 48 -sc_threshold 0 -keyint_min 48 \
+-map [v3out] -c:v:3 libx264 -x264-params "nal-hrd=cbr:force-cfr=1" -b:v:3 0.3M -maxrate:v:3 0.3M -minrate:v:3 0.3M -bufsize:v:3 0.5M -preset fast -g 48 -sc_threshold 0 -keyint_min 48 \
+-map a:0 -c:a:1 aac -b:a:1 96k -ac 2 \
+-map a:0 -c:a:2 aac -b:a:2 64k -ac 2 \
+-map a:0 -c:a:3 aac -b:a:3 38k -ac 2 \
+-f hls \
+-hls_time 2 \
+-hls_playlist_type vod \
+-hls_flags independent_segments \
+-hls_segment_type mpegts \
+-hls_segment_filename stream_%v_data%02d.ts \
+-master_pl_name master.m3u8 \
+-var_stream_map "v:0,a:0 v:1,a:1 v:2,a:2" stream_%v.m3u8
--- src/index.mjs
+++ src/index.mjs
@@ -1,8 +1,14 @@
 #!/usr/bin/env node
+
 const WEB_3_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweEI5YTAxYTI1MjE2MTJkMjY2NDQ4NDMyMjlGMzk2QzljNzU0N0IyY0IiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2Mzk5MzIyMTM3NzAsIm5hbWUiOiJrZXNoYXR2LXVwbG9hZGVyIn0.gpB0pHd9apCG_H3BAiI65LSdqbd9F3TVdiWRE1FBf9I"
 
 import {getFilesFromPath} from 'web3.storage'
 import {Web3Storage} from 'web3.storage'
+import { packToFs } from 'ipfs-car/pack/fs'
+import { FsBlockStore } from 'ipfs-car/blockstore/fs'
+import { CarIndexedReader } from '@ipld/car'
+
+const carPath = `${process.cwd()}/../output.car`
 
 function getAccessToken() {
     return WEB_3_TOKEN
@@ -30,12 +36,24 @@ async function storeWithProgress(files) {
         process.stdout.write(`Uploading... ${pct.toFixed(2)}% complete\r`)
     }
     const client = makeStorageClient()
-    return client.put(files, {onRootCidReady, onStoredChunk, wrapWithDirectory: false})
+    console.log("start pack files");
+    const a = await packToFs({
+        input: process.cwd(),
+        output: carPath,
+        blockstore: new FsBlockStore(),
+        wrapWithDirectory: false,
+    });
+    onRootCidReady(a.root.toString())
+    const car = await CarIndexedReader.fromFile(carPath)
+    return client.putCar(car, {onStoredChunk})
 }
 
 async function run() {
     return getFiles(process.cwd()).then(files => {
         return storeWithProgress(files)
+    }).then(value => {
+        console.log("done upload", value);
+        return value;
     });
 }
 
